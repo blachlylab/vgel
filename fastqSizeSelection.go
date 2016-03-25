58d17b458bf1c877f5ed36c5b3eb2a5898cee3eb
James Blachly <james.blachly@gmail.com>
2016-03-25 16:27:25 -0400

    changed to 4-line records to correct output

--- fastqSizeSelection.go
+++ fastqSizeSelection.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"compress/gzip"
 	"flag"
 	"fmt"
 	"os"
@@ -11,55 +10,80 @@ import (
 
 var wg sync.WaitGroup;
 
-func readGzFile(filename string, inputlines chan <- string) {
+type FastQrecord struct {
+	header string
+	sequence string
+	bonus string
+	quality string
+}
+
+func readGzFile(filename string, inputlines chan <- FastQrecord) {
 	// read the gzipped input file
 	fi, err := os.Open(filename)
 	if err != nil {
 		panic(err)
 	}
 	defer fi.Close()
+	/*
 	fz, err := gzip.NewReader(fi)
 	if err != nil {
 		panic(err)
 	}
 	defer fz.Close()
+	*/
 
-
-	scanner := bufio.NewScanner(fz)
+	scanner := bufio.NewScanner(fi)
 	// Set up a fixed buffer to avoid allocations
 	scanbuf := make([]byte, 4096);
 	scanner.Buffer(scanbuf, 4096);
 
 	for scanner.Scan() {
-		inputlines <- scanner.Text()
+		fqrecord := FastQrecord{"", "", "", ""}
+		fqrecord.header = scanner.Text()
+		
+		scanner.Scan()
+		fqrecord.sequence = scanner.Text()
+		
+		scanner.Scan()
+		fqrecord.bonus = scanner.Text()
+		
+		scanner.Scan()
+		fqrecord.quality = scanner.Text()
+		
+		inputlines <- fqrecord
 	}
 	close(inputlines)
 }
 
-func checkLines(minLen int, maxLen int, inputlines <- chan string, passedlines chan <- string) {
-	for fqline := range inputlines {
-		if len(fqline) >= minLen && len(fqline) <= maxLen {
-			passedlines <- fqline
+func checkLines(minLen int, maxLen int, inputlines <- chan FastQrecord, passedlines chan <- FastQrecord) {
+	for fqrecord := range inputlines {
+		if len(fqrecord.sequence) >= minLen && len(fqrecord.sequence) <= maxLen {
+			passedlines <- fqrecord
 		}
 	}
 	close(passedlines)
 }
 
-func writeGzFile(outFile string, passedlines <- chan string) {
+func writeGzFile(outFile string, passedlines <- chan FastQrecord) {
 	// init writing the gzipped output file
 	fo, err := os.Create(outFile)
 	if err != nil {
 		panic(err)
 	}
 	defer fo.Close()
+	/*
 	fzo := gzip.NewWriter(fo)
 	if err != nil {
 		panic(err)
 	}
 	defer fzo.Close()
-	
-	for fqline := range passedlines {
-		fzo.Write( []byte(fqline + "\n") );
+	*/
+
+	for fqrecord := range passedlines {
+		fo.Write( []byte(fqrecord.header + "\n") );
+		fo.Write( []byte(fqrecord.sequence + "\n") );
+		fo.Write( []byte(fqrecord.bonus + "\n") );
+		fo.Write( []byte(fqrecord.sequence + "\n") );
 	}
 
 	// release the Wait() in main()
@@ -78,8 +102,8 @@ func main() {
 		abort(err)
 	}
 	
-	inputlines := make(chan string, 400)
-	passedlines := make(chan string, 400)
+	inputlines := make(chan FastQrecord, 16000)
+	passedlines := make(chan FastQrecord, 16000)
 
 	info("processing " + *flagFastq)
 	
